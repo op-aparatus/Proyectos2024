6b13750a5ac03be438fa200ab1401c270e7c78f3
aparatus <opichardop@gmail.com>
2024-08-27 09:10:25 -0600

    Add files via upload

---
+++ data/CIRCUITO10POLIGONALCERRADAJLISTO_5.js
@@ -0,0 +1,1 @@
+var json_CIRCUITO10POLIGONALCERRADAJLISTO_5 = {"type":"FeatureCollection","name":"CIRCUITO10POLIGONALCERRADAJLISTO_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
